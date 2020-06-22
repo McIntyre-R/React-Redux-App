97d155eb9059bc8353628650887d4431bf291f76
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-06-22 10:47:39 -0400

    chart fix

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -2,45 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import FreeCompany from './FreeCompany'
 import FriendsList from './FriendsList'
-import Chart from 'chart.js'
 
-var ctx = 'myChart';
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-})
 
 function Character(props){
     console.log(props)
@@ -55,7 +17,6 @@ return(
                 <h6>({props.characterInfo.DC})</h6>
                 <br />
                 <p>Current Job: {props.characterInfo.ActiveClassJob.Name} lvl.{props.characterInfo.ActiveClassJob.Level}</p>
-                <canvas id="myChart" width="400" height="400"></canvas>
             </div>  
         </div>
         <div className='flex wrap' >
